1cc2b83670db7e4aa8a8be2b1c22deafa698c501
sadmiraga <sadmirvela@gmail.com>
2019-04-06 21:24:52 +0200

    init

---
+++ Binary Search Practice/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Binary_Search_Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.minButton = new System.Windows.Forms.Button();
+            this.vsotaButton = new System.Windows.Forms.Button();
+            this.maxButton = new System.Windows.Forms.Button();
+            this.poprecjeButton = new System.Windows.Forms.Button();
+            this.dodajButton = new System.Windows.Forms.Button();
+            this.vsebujeButton = new System.Windows.Forms.Button();
+            this.vsebujeBinarnoButton = new System.Windows.Forms.Button();
+            this.urediIzbiranjemButton = new System.Windows.Forms.Button();
+            this.urediVstavljanjemButton = new System.Windows.Forms.Button();
+            this.urediMehurckiButton = new System.Windows.Forms.Button();
+            this.hitroUrediButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // minButton
+            // 
+            this.minButton.Location = new System.Drawing.Point(37, 79);
+            this.minButton.Name = "minButton";
+            this.minButton.Size = new System.Drawing.Size(100, 48);
+            this.minButton.TabIndex = 1;
+            this.minButton.Text = "Min";
+            this.minButton.UseVisualStyleBackColor = true;
+            // 
+            // vsotaButton
+            // 
+            this.vsotaButton.Location = new System.Drawing.Point(37, 133);
+            this.vsotaButton.Name = "vsotaButton";
+            this.vsotaButton.Size = new System.Drawing.Size(100, 48);
+            this.vsotaButton.TabIndex = 2;
+            this.vsotaButton.Text = "Vsota";
+            this.vsotaButton.UseVisualStyleBackColor = true;
+            // 
+            // maxButton
+            // 
+            this.maxButton.Location = new System.Drawing.Point(143, 79);
+            this.maxButton.Name = "maxButton";
+            this.maxButton.Size = new System.Drawing.Size(100, 48);
+            this.maxButton.TabIndex = 3;
+            this.maxButton.Text = "Max";
+            this.maxButton.UseVisualStyleBackColor = true;
+            // 
+            // poprecjeButton
+            // 
+            this.poprecjeButton.Location = new System.Drawing.Point(143, 133);
+            this.poprecjeButton.Name = "poprecjeButton";
+            this.poprecjeButton.Size = new System.Drawing.Size(100, 48);
+            this.poprecjeButton.TabIndex = 4;
+            this.poprecjeButton.Text = "Poprečje";
+            this.poprecjeButton.UseVisualStyleBackColor = true;
+            // 
+            // dodajButton
+            // 
+            this.dodajButton.Location = new System.Drawing.Point(143, 25);
+            this.dodajButton.Name = "dodajButton";
+            this.dodajButton.Size = new System.Drawing.Size(100, 48);
+            this.dodajButton.TabIndex = 5;
+            this.dodajButton.Text = "dodaj";
+            this.dodajButton.UseVisualStyleBackColor = true;
+            // 
+            // vsebujeButton
+            // 
+            this.vsebujeButton.Location = new System.Drawing.Point(249, 25);
+            this.vsebujeButton.Name = "vsebujeButton";
+            this.vsebujeButton.Size = new System.Drawing.Size(100, 48);
+            this.vsebujeButton.TabIndex = 6;
+            this.vsebujeButton.Text = "Vsebuje";
+            this.vsebujeButton.UseVisualStyleBackColor = true;
+            // 
+            // vsebujeBinarnoButton
+            // 
+            this.vsebujeBinarnoButton.Location = new System.Drawing.Point(355, 25);
+            this.vsebujeBinarnoButton.Name = "vsebujeBinarnoButton";
+            this.vsebujeBinarnoButton.Size = new System.Drawing.Size(146, 79);
+            this.vsebujeBinarnoButton.TabIndex = 7;
+            this.vsebujeBinarnoButton.Text = "Vsebuje binarno";
+            this.vsebujeBinarnoButton.UseVisualStyleBackColor = true;
+            // 
+            // urediIzbiranjemButton
+            // 
+            this.urediIzbiranjemButton.Location = new System.Drawing.Point(355, 110);
+            this.urediIzbiranjemButton.Name = "urediIzbiranjemButton";
+            this.urediIzbiranjemButton.Size = new System.Drawing.Size(146, 79);
+            this.urediIzbiranjemButton.TabIndex = 8;
+            this.urediIzbiranjemButton.Text = "Uredi z izbiranjem";
+            this.urediIzbiranjemButton.UseVisualStyleBackColor = true;
+            // 
+            // urediVstavljanjemButton
+            // 
+            this.urediVstavljanjemButton.Location = new System.Drawing.Point(355, 195);
+            this.urediVstavljanjemButton.Name = "urediVstavljanjemButton";
+            this.urediVstavljanjemButton.Size = new System.Drawing.Size(146, 79);
+            this.urediVstavljanjemButton.TabIndex = 9;
+            this.urediVstavljanjemButton.Text = "Uredi z vstavljanjem";
+            this.urediVstavljanjemButton.UseVisualStyleBackColor = true;
+            // 
+            // urediMehurckiButton
+            // 
+            this.urediMehurckiButton.Location = new System.Drawing.Point(355, 280);
+            this.urediMehurckiButton.Name = "urediMehurckiButton";
+            this.urediMehurckiButton.Size = new System.Drawing.Size(146, 79);
+            this.urediMehurckiButton.TabIndex = 10;
+            this.urediMehurckiButton.Text = "Uredi z mehurčki";
+            this.urediMehurckiButton.UseVisualStyleBackColor = true;
+            // 
+            // hitroUrediButton
+            // 
+            this.hitroUrediButton.Location = new System.Drawing.Point(355, 365);
+            this.hitroUrediButton.Name = "hitroUrediButton";
+            this.hitroUrediButton.Size = new System.Drawing.Size(146, 79);
+            this.hitroUrediButton.TabIndex = 11;
+            this.hitroUrediButton.Text = "Hitro uredi";
+            this.hitroUrediButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hitroUrediButton);
+            this.Controls.Add(this.urediMehurckiButton);
+            this.Controls.Add(this.urediVstavljanjemButton);
+            this.Controls.Add(this.urediIzbiranjemButton);
+            this.Controls.Add(this.vsebujeBinarnoButton);
+            this.Controls.Add(this.vsebujeButton);
+            this.Controls.Add(this.dodajButton);
+            this.Controls.Add(this.poprecjeButton);
+            this.Controls.Add(this.maxButton);
+            this.Controls.Add(this.vsotaButton);
+            this.Controls.Add(this.minButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button minButton;
+        private System.Windows.Forms.Button vsotaButton;
+        private System.Windows.Forms.Button maxButton;
+        private System.Windows.Forms.Button poprecjeButton;
+        private System.Windows.Forms.Button dodajButton;
+        private System.Windows.Forms.Button vsebujeButton;
+        private System.Windows.Forms.Button vsebujeBinarnoButton;
+        private System.Windows.Forms.Button urediIzbiranjemButton;
+        private System.Windows.Forms.Button urediVstavljanjemButton;
+        private System.Windows.Forms.Button urediMehurckiButton;
+        private System.Windows.Forms.Button hitroUrediButton;
+    }
+}
+
---
+++ Binary Search Practice/Tab.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Practice
+{
+    class Tab
+    {
+        private int[] tabela = new int[100];
+        int dolzina = 0;
+
+        //add new value to array and increase value 'dolzina'
+        void dodaj(int x)
+        {
+            tabela[dolzina] = x;
+            dolzina++;
+        }
+
+        //TO DO 
+        // VSEBUJE
+
+        //convert whole array to string
+        string ToString2()
+        {
+            string temp = "";
+
+            for(int i=0; i <= dolzina-1; i++)
+            {
+                temp += tabela[i].ToString() + ",";
+            }
+
+            return temp;
+        }
+
+        //VSOTA -- returns value of whole 
+        int vsota()
+        {
+            int value = 0;
+
+            for (int i = 0; i <= dolzina - 1; i++)
+            {
+                value += tabela[i];
+            }
+
+            return value;
+        }
+
+        //MIN -- returns smallest number from array
+        int Min()
+        {
+            //lets say first number is the smallest
+            int minIndex = 0;
+
+            for(int i =1; i <= dolzina - 1; i++)
+            {
+                if (tabela[i] < tabela[minIndex]) minIndex = i;
+            }
+
+            return tabela[minIndex];
+        }
+
+        //MAX -- returns biggest number from array
+        int Max()
+        {
+            //let's say first number is the biggest
+            int maxIndex = 0;
+
+            for (int i = 1; i <= dolzina - 1; i++)
+            {
+                if (tabela[i] > tabela[maxIndex]) maxIndex = i;
+            }
+
+            return tabela[maxIndex];
+        }
+
+
+    }
+}
